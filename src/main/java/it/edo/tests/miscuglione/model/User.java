a185e3b1f6287986626f7022600150640fd83001
edoardocampagnano <registrazioni.edoardo@gmail.com>
2020-11-15 21:21:37 +0100

    versione funzionante

--- src/main/java/it/edo/tests/miscuglione/model/User.java
+++ src/main/java/it/edo/tests/miscuglione/model/User.java
@@ -1,2 +1,20 @@
-package it.edo.tests.miscuglione.model;public class User {
+package it.edo.tests.miscuglione.model;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String userName;
 }
---
+++ src/test/java/it/edo/tests/miscuglione/MiscuglioneApplicationTests.java
@@ -0,0 +1,13 @@
+package it.edo.tests.miscuglione;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiscuglioneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
